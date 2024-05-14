af9df59dd2a07e82cd507417bdc760d5b70a0079
B3rX <ed.esteban.ruiz@gmail.com>
2024-05-13 20:36:28 -0500

    Updated User creation

--- login-mf/src/api/UserCreationApi.js
+++ login-mf/src/api/UserCreationApi.js
@@ -3,26 +3,18 @@ export const createUser = ({ username, email, password, firstName = "", lastName
   myHeaders.append("Content-Type", "application/json");
   myHeaders.append("Authorization", `Bearer ${token}`);
 
-  // const rawBody = JSON.stringify({
-  //   "username": username,
-  //   "email": email,
-  //   "firstName": firstName,
-  //   "lastName": lastName,
-  //   "password": password,
-  // });  
-
-  const raw = JSON.stringify({
-    "username": "andrea.calle",
-    "email": "andrea@mail.com",
-    "firstName": "Andrea",
-    "lastName": "Calle",
-    "password": "1234"
-  });
+  const rawBody = JSON.stringify({
+    "username": username,
+    "email": email,
+    "firstName": firstName,
+    "lastName": lastName,
+    "password": password,
+  });  
 
   const requestOptions = {
     method: "POST",
     headers: myHeaders,
-    body: raw,
+    body: rawBody,
     redirect: "follow"
   };
 
