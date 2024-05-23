0e83f9a7f1b44697aadfdb503d47fc70784efbc4
TheB3rX <ed.esteban.ruiz@gmail.com>
2024-05-23 16:45:25 -0500

    Updated URL's, now it should be working

--- turn-host/src/App.jsx
+++ turn-host/src/App.jsx
@@ -6,8 +6,8 @@ import { ProtectedRoute } from "./routes/ProtectedRoute";
 import { SignupPage } from "./pages/SignupPage";
 import { AdminTurnsPage } from "./pages/AdminTurnsPage";
 import { CreatePage } from "./pages/CreatePage";
-import { EditPage } from "./pages/EditPage";
 import { DeletePage } from "./pages/DeletePage";
+import { IdentitySignupPage } from "./pages/IdentitySignupPage";
 
 
 const App = () => (
@@ -17,9 +17,10 @@ const App = () => (
       <Route path="/turns" element={<TurnsPage />} />
       <Route path="/adminTurns" element={<AdminTurnsPage/>} />
       <Route path="/create" element={<CreatePage/>} />
-      <Route path="/edit" element={<EditPage/>} />
       <Route path="/delete" element={<DeletePage/>} />
     </Route>
+    {/* For when someone has identity signed up and havent completed information */}
+    <Route element={<IdentitySignupPage/>} path="/complete-information" exact />
     <Route element={<SignupPage/>} path="/signup" exact />
   </Routes>
 );
